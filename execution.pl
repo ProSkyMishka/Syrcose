aa964c30829677166665bfbf7ff285cf911d9a45
ProSkyMishka <mprozorskiy@mail.ru>
2025-04-11 03:04:18 +0300

    Add autoupdate soil_moisture level until 15 seconds

--- execution.pl
+++ execution.pl
@@ -3,7 +3,7 @@ execute_watering(Plant) :-
     command_allowed(controller_1, water(Plant)),
     select_watering_agent(Plant, Agent),
     format("~w is watering ~w.~n", [Agent, Plant]),
-    update_moisture_after_watering(Plant).
+    update_moisture_async(Plant).
 
 execute_watering(Plant) :-
     \+ command_allowed(controller_1, water(Plant)),
@@ -13,9 +13,19 @@ next_moisture_level(low, medium).
 next_moisture_level(medium, high).
 next_moisture_level(high, high). 
 
-update_moisture_after_watering(Plant) :-
-    soil_moisture(Plant, Current),
-    next_moisture_level(Current, Next),
-    retract(soil_moisture(Plant, Current)),
-    assert(soil_moisture(Plant, Next)),
-    format("Soil moisture of ~w updated: ~w → ~w~n", [Plant, Current, Next]).
+update_moisture_async(Plant) :-
+    retract(soil_moisture(Plant, low)),
+    assert(soil_moisture(Plant, medium)),
+    format('Moisture is now medium for ~w.~n', [Plant]),
+    % Отложим выполнение на 60 секунд в отдельном потоке
+    thread_create(wait_and_update(Plant), _, [detached(true)]).
+
+wait_and_update(Plant) :-
+    sleep(15),
+    retract(soil_moisture(Plant, medium)),
+    assert(soil_moisture(Plant, low)),
+    format('Moisture is now low for ~w again.~n', [Plant]).
+
+check_soil_moisture(Plant) :-
+    soil_moisture(Plant, Moisture),
+    format('Current moisture level for ~w: ~w~n', [Plant, Moisture]).
